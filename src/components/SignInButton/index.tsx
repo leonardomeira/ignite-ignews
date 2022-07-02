a0de7e9a0b857188d6696d327a0b6955f34ce06c
Leonardo Meira <leonardo@casadasfechaduras.br>
2022-07-02 11:31:37 -0300

    implement user data saving

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -14,7 +14,7 @@ export function SignInButton() {
             className={styles.signInButton}
             onClick={() => signOut()}
         >
-            <FaGithub color={"#04d361"} />
+            <FaGithub color="#04d361" />
             {session.user.name}
             <FiX color="#737388" className={styles.closeIcon} />
         </button>
@@ -24,7 +24,7 @@ export function SignInButton() {
             className={styles.signInButton}
             onClick={() => signIn('github')}
         >
-            <FaGithub color={"#eba417"} />
+            <FaGithub color="#eba417" />
             Sign in with Github
         </button>
     );
