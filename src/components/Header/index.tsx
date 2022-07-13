731ed0f4f9dd983ba861e77bae341d683e13e7b4
Leonardo Meira <leonardo@casadasfechaduras.br>
2022-07-13 17:38:10 -0300

    add route highlight

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <a href="/"><img src="/images/logo.svg" alt="ig.news" /></a>
+                <ActiveLink href="/"><a><img src="/images/logo.svg" alt="ig.news" /></a></ActiveLink>
                 <nav>
-                    <a className={styles.active} href="#">Home</a>
-                    <a href="#">Posts</a>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active}>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
